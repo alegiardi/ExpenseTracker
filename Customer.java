f9d6467ed5c5ca170049ac47afecddd91e9bde2c
Alessia G. <aag228@bath.ac.uk>
2024-03-15 17:15:27 +0000

    Building blocks for ExpenseTracker

---
+++ Customer.java
@@ -0,0 +1,8 @@
+public class Customer {
+ 
+    // logic for client with multiple accounts
+
+    // ArrayList with all the client's accounts
+
+    // login details required, username & password
+}
---
+++ Transactions.java
@@ -0,0 +1,17 @@
+
+// Class to handle transactions, subtracting and adding to a bank
+public class Transactions {
+
+    public Transactions(){
+        // must have access to bank accounts
+        // affected balance should be of current account unless a transaction uses savings
+    }
+    
+    public void transaction(double amount){
+        // balance - amount
+    }
+
+    public void payment(double amount){
+        // balance + amount
+    }
+}
